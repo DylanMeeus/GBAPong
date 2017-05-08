5b78574187a2e2f69e2d38aeb21376371c6dc960
Dylan Meeus <meeusdylan@hotmail.com>
2017-05-08 19:16:25 +0200

    GBA variables

---
+++ code/gba.h
@@ -0,0 +1,13 @@
+#ifndef GBA_H
+#define GBA_H
+
+#define REG_DISPLAYCONTROL *((volatile uint32*) (0x04000000))
+#define VIDEO_MODE  0x0003
+#define BG_MODE  0x0400
+#define SCREEN_BUFFER ((volatile uint16*)0x06000000)
+#define SCREEN_WIDTH 240
+#define SCREEN_HEIGHT 160
+#define REG_VCOUNT      (* (volatile uint16*) 0x04000006) // current row being drawn by hardware
+
+
+#endif
